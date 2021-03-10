0f21cdf8b5335e99ae687e85b581948d12a6d35e
meaqese <meaqese@protonmail.com>
2021-03-10 22:07:16 +0300

    module1-task3

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const MovieCard = ({data: {name}}) => {
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={name}
+          width="280" height="175"/>
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html">{name}</a>
+      </h3>
+    </article>
+  );
+};
+
+MovieCard.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    date: PropTypes.number.isRequired
+  })
+};
+
+export default MovieCard;
+
